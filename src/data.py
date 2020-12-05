ae3493150ba811c48bf82801db1a108247ad7eee
SimonMaracine <simonmaracine@gmail.com>
2020-12-05 20:59:44 +0200

    Add GUI window to the application and update .gitignore

---
+++ src/data.py
@@ -0,0 +1,85 @@
+import re
+from datetime import datetime
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Message:
+    date_time: datetime
+    person_name: str
+    message: str
+
+
+def read_chat_file(file: str, messages: list[Message]):
+    with open(file, "r") as file:
+        while line := file.readline():
+            message = _parse_line(line, messages)
+            if message is not None:
+                messages.append(message)
+                print(message)
+
+
+def _parse_line(line: str, messages: list[Message]) -> Optional[Message]:
+    # Check to see if this is a message line
+    match = re.match("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}, [0-9]{1,2}:[0-9]{2} (PM|AM) - ", line)
+
+    # If there is no match, then this line is the continuation of the last message
+    if match is None:
+        messages[-1].message += "\n" + line.rstrip("\n")
+        return None
+
+    hypen_pos = line.find(" - ")
+    if hypen_pos == -1:
+        raise RuntimeError("Chat file is corrupted")
+
+    message_info_str = line[0:hypen_pos]
+    message_info: list = message_info_str.split(" ")
+    date: str = message_info[0].rstrip(",")
+    time: str = message_info[1]
+    meridian: str = message_info[2]
+
+    month, day, year = list(map(int, date.split("/")))
+    year += 2000
+    hour, minute = _parse_12_format_time(time, meridian)
+
+    date_time = datetime(year, month, day, hour, minute)
+
+    person_and_message: str = line[hypen_pos + 3:]
+    delimiter = person_and_message.find(": ")
+
+    # If there is no delimeter, then this line is not a message, so skip it
+    if delimiter == -1:
+        return None
+
+    person_name = person_and_message[0:delimiter]
+    message = person_and_message[delimiter + 2:].rstrip("\n")
+
+    return Message(date_time, person_name, message)
+
+
+def _parse_12_format_time(time: str, meridian: str) -> tuple[int, int]:
+    colon_pos = time.find(":")
+    if colon_pos == -1:
+        raise RuntimeError("Chat file is corrupted")
+
+    try:
+        hour12 = int(time[0:colon_pos])
+        minute12 = int(time[colon_pos + 1:])
+    except ValueError:
+        raise RuntimeError("Chat file is corrupted")
+
+    if meridian == "AM":
+        if hour12 == 12:
+            hour24 = hour12 - 12
+        else:
+            hour24 = hour12
+    elif meridian == "PM":
+        if 11 >= hour12 >= 1:
+            hour24 = hour12 + 12
+        else:
+            hour24 = hour12
+    else:
+        raise RuntimeError("Chat file is corrupted")
+
+    return hour24, minute12
--- src/main.py
+++ src/main.py
@@ -1,100 +1,47 @@
-import sys
-import re
-from datetime import datetime
-from dataclasses import dataclass
-from typing import Optional
+import tkinter as tk
+
+from data import read_chat_file, Message
 
 
-@dataclass
-class Message:
-    date_time: datetime
-    person_name: str
-    message: str
+class MainApplication(tk.Frame):
+
+    def __init__(self, root: tk.Tk):
+        super().__init__(root)
+        self.root = root
+        self.pack(fill="both", expand=True, padx=30, pady=30)
+
+        self.root.option_add("*tearOff", False)
+        self.root.title("WhatsApp Chat Reader")
+
+        men_file = tk.Menu(self)
+        men_file.add_command(label="Open Chat", command=None)
+
+        men_help = tk.Menu(self)
+        men_help.add_command(label="About", command=None)
+
+        men_main = tk.Menu(self)
+        men_main.add_cascade(label="File", menu=men_file)
+        men_main.add_cascade(label="Help", menu=men_help)
+
+        # Main frames
+        frm_people = tk.Frame(self, relief="ridge", bd=3)
+        frm_people.grid(row=0, column=0)
+
+        frm_right_side = tk.Frame(self)
+        frm_right_side.grid(row=0, column=1)
+
+        tk.Label(frm_people, text="Simon - 100 messages").pack()
+
+        self.messages: list[Message] = []
+
+        read_chat_file("../chats/WhatsApp Chat with +40 758 628 378.txt", self.messages)
 
 
-def read_chat_file(file: str, messages: list[Message]):
-    with open(file, "r") as file:
-        while line := file.readline():
-            message = parse_line(line, messages)
-            if message is not None:
-                messages.append(message)
-                print(message)
-
-
-def parse_line(line: str, messages: list[Message]) -> Optional[Message]:
-    # Check to see if this is a message line
-    match = re.match("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}, [0-9]{1,2}:[0-9]{2} (PM|AM) - ", line)
-
-    # If there is no match, then this line is the continuation of the last message
-    if match is None:
-        messages[-1].message += "\n" + line.rstrip("\n")
-        return None
-
-    hypen_pos = line.find(" - ")
-    if hypen_pos == -1:
-        raise RuntimeError("Chat file is corrupted")
-
-    message_info_str = line[0:hypen_pos]
-    message_info: list = message_info_str.split(" ")
-    date: str = message_info[0].rstrip(",")
-    time: str = message_info[1]
-    meridian: str = message_info[2]
-
-    month, day, year = list(map(int, date.split("/")))
-    year += 2000
-    hour, minute = parse_12_format_time(time, meridian)
-
-    date_time = datetime(year, month, day, hour, minute)
-
-    person_and_message: str = line[hypen_pos + 3:]
-    delimiter = person_and_message.find(": ")
-
-    # If there is no delimeter, then this line is not a message, so skip it
-    if delimiter == -1:
-        return None
-
-    person_name = person_and_message[0:delimiter]
-    message = person_and_message[delimiter + 2:].rstrip("\n")
-
-    return Message(date_time, person_name, message)
-
-
-def parse_12_format_time(time: str, meridian: str) -> tuple[int, int]:
-    colon_pos = time.find(":")
-    if colon_pos == -1:
-        raise RuntimeError("Chat file is corrupted")
-
-    try:
-        hour12 = int(time[0:colon_pos])
-        minute12 = int(time[colon_pos + 1:])
-    except ValueError:
-        raise RuntimeError("Chat file is corrupted")
-
-    if meridian == "AM":
-        if hour12 == 12:
-            hour24 = hour12 - 12
-        else:
-            hour24 = hour12
-    elif meridian == "PM":
-        if 11 >= hour12 >= 1:
-            hour24 = hour12 + 12
-        else:
-            hour24 = hour12
-    else:
-        raise RuntimeError("Chat file is corrupted")
-
-    return hour24, minute12
-
-
-def main(args: list):
-    if len(args) < 2:
-        print("Please pass the chat file to the program")
-        return
-
-    messages: list[Message] = []
-
-    read_chat_file(args[1], messages)
+def main():
+    root = tk.Tk()
+    MainApplication(root)
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
